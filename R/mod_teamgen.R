12844b86f1f51ab7b22b329b5d73f7830200591c
Adam Peterson <adampeterson@adams-air.lan>
2023-03-05 11:33:47 -0500

    Add in functionality for stratified team balance

--- R/mod_teamgen.R
+++ R/mod_teamgen.R
@@ -29,6 +29,11 @@ mod_teamgen_ui <- function(id) {
           max = 15,
           value = 7
         ),
+        radioButtons(
+          inputId = NS(id, "stratify"),
+          label = "Stratify Team Balance",
+          c("No" = FALSE, "Yes" = TRUE)
+        ),
         tags$a(
           id = NS(id, "downloadgit"),
           class = paste("btn btn-default shiny-download-link", "btn-primary"),
@@ -57,7 +62,7 @@ mod_teamgen_server <- function(id) {
       filename = function() {"PlayerTemplate.csv"},
       content = function(file) {
         write.table(
-          x = balancedteams::mens_team, file = file, quote = TRUE, sep = ",",
+          x = balancedteams::stratified_dataset, file = file, quote = TRUE, sep = ",",
           row.names = FALSE, col.names = TRUE)
          }
        )
@@ -76,29 +81,42 @@ mod_teamgen_server <- function(id) {
       req(file)
       ext <- tools::file_ext(file$datapath)
       validate(need(ext == "csv", "Please upload a csv file"))
-      indf <- read.csv(file$datapath)
+      indf <- readr::read_csv(file$datapath)
     })
 
     team_id <- group_id <- score <- group_score <- NULL
 
     nt <- reactive({input$num_teams})
 
+    stratify_opt <- reactive({as.logical(input$stratify)})
+
     gtdf <-
       reactive({
         req(input$upload)
-        balancedteams::GenerateBalancedTeams(indf()$group_id,
-                                             indf()$group_score,
-                                             num_teams = nt()) %>%
-        dplyr::arrange(team_id, group_id, score)
+        balancedteams::GenerateBalancedTeams(indf(),
+                                             num_teams = nt(),
+                                             stratify = stratify_opt()) # %>%
+        # dplyr::arrange(team_id, player_id, player_score)
       })
 
     st <- reactive({
-      gtdf() %>%
-      dplyr::group_by(team_id) %>%
-      dplyr::summarize(`Mean Score` = mean(score),
-                       `# Players` = dplyr::n()) %>%
-      dplyr::ungroup() %>%
-      dplyr::rename(`Team` = team_id)
+      if(stratify_opt()) {
+        gtdf() %>%
+        dplyr::group_by(team_id) %>%
+        dplyr::summarize(`Mean Score` = mean(player_score),
+                         `Mean Strata Score` = mean(player_score * strata),
+                         `# Players` = dplyr::n(),
+                         `# Strata Players` = sum(strata)) %>%
+        dplyr::rename(Team = team_id)
+
+      } else {
+        gtdf() %>%
+        dplyr::group_by(team_id) %>%
+        dplyr::summarize(`Mean Score` = mean(player_score),
+                         `# Players` = dplyr::n()) %>%
+        dplyr::ungroup() %>%
+        dplyr::rename(`Team` = team_id)
+      }
     })
 
     output$summary_table <- renderTable({st()})
